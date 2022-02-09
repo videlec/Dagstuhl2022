7941622806554a1c7d3681b9865f77ca18046b10
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2022-02-09 20:38:08 +0100

    more generic layout code

--- Problem8-SquareTiledSurfaces/layout.py
+++ Problem8-SquareTiledSurfaces/layout.py
@@ -1,23 +1,40 @@
 r"""
 The layout problem for square-tiled surfaces
+
+Square-tiled surfaces are encoded as pairs ``(r, u)`` where both ``r`` and
+``u`` are lists of integers corresponding to the permutations ``i -> r[i]``
+and ``i -> u[i]``.
 """
 
 from sage.numerical.mip import MIPSolverException
 
+def origami_digraph(r, u, loops=True, multiedges=True):
+    n = len(r)
+    G = Digraph(loops=loops, multiedges=multiedges)
+    for i in range(n):
+        if loops or r[i] != i:
+            G.add_edge(i, r[i], 'r')
+        if loops or u[i] != i:
+            G.add_edge(i, u[i], 'u')
+    return G
+
+
+def staircase_layout(r, u):
+    r"""
+    Return a staircase layout if any
+    """
+    G = origami_digraph(r, u, loops=False, multiedges=False)
+    H = G.hamiltonian_path(maximize=True)
+    return H if (H is not None and len(H) == len(G)) else None
+
+
 def soft_layout(r, u):
     r"""
-    Return a soft layout as a 4-tuple ``(Graph, x_pos, y_pos, r_cut, u_cut)``
-    for the origami ``(r,u)`` or ``None`` if there is no such layout.
+    Return a soft layout if any
 
-    - ``Graph`` - the subgraph used in the layout
-
-    - ``x_pos`` (list of ``n`` integers) - x coordinates of each square
-
-    - ``y_pos`` (list of ``n`` integers) - y coordinates of each square
-
-    - ``r_cut`` (list of ``n`` booleans) - positions of the horizontal cuts
-
-    - ``u_cut`` (list of ``n`` booleans) - positions of the vertical cuts
+    This function uses integer linear programming and it is strongly advised to
+    use a decent solver. The only one installed by default in sage (GLPK) turns
+    out to be very slow.
     """
     n = len(r)
     r_inv = [None] * n
@@ -90,9 +107,9 @@ def soft_layout(r, u):
                   M.sum(u_cut[i] for i in comp if u[i] not in comp) + \
                   M.sum(u_cut[u_inv[i]] for i in comp if u_inv[i] not in comp)
             M.add_constraint(cut >= 1)
-            continue
 
         # forbid squares at the same position if any
+        # (we avoid doing that too much since this potentially creates 2 n^2 new variables)
         has_superposition = False
         for i in range(n):
             for j in range(i):
@@ -102,19 +119,72 @@ def soft_layout(r, u):
                     M.add_constraint(x[i] - x[j] + 3 * n * xdiff[i,j] + y[j] - y[i] + 3 * n * (1 - ydiff[i,j]) >= 1)
                     M.add_constraint(x[j] - x[i] + 3 * n * (1 - xdiff[i,j]) + y[j] - y[i] + 3 * n * (1 - ydiff[i,j]) >= 1)
                     has_superposition = True
-                if has_superposition:
                     break
             if has_superposition:
                 break
 
-        if not has_superposition:
-            vx = M.get_values(x)
-            vy = M.get_values(y)
-            return G, [int(vx[i]) for i in range(n)], [int(vy[i]) for i in range(n)], [bool(vr_cut[i]) for i in range(n)], [bool(vu_cut[i]) for i in range(n)]
+        if len(comps) == 1 and not has_superposition:
+            G = DiGraph(n, multiedges=False, loops=False)
+            for i in range(n):
+                if not vr_cut[i] and i != r[i]:
+                    G.add_edge(i, r[i], 'r')
+                if not vu_cut[i] and i != u[i]:
+                    G.add_edge(i, u[i], 'u')
+            return G
 
-def soft_plot(r, u):
+############
+# Plotting #
+############
+
+def positions_from_layout(r, u, G):
+    n = len(r)
+    x_pos = [None] * n
+    y_pos = [None] * n
+    x_pos[0] = y_pos[0] = 0
+    todo = [0]
+    while todo:
+        i = todo.pop()
+        for (_, j, lab) in G.outgoing_edges(i, labels=True):
+            if lab == 'r':
+                assert r[i] == j
+            elif lab == 'u':
+                assert u[i] == j
+            else:
+                raise RuntimeError
+
+            if x_pos[j] is None:
+                assert y_pos[j] is None
+                if lab == 'r':
+                    x_pos[j] = x_pos[i] + 1
+                    y_pos[j] = y_pos[i]
+                else:
+                    x_pos[j] = x_pos[i]
+                    y_pos[j] = y_pos[i] + 1
+                todo.append(j)
+        for (j, _, lab) in G.incoming_edges(i, labels=True):
+            if lab == 'r':
+                assert r[j] == i
+            elif lab == 'u':
+                assert u[j] == i
+            else:
+                raise RuntimeError
+
+            if x_pos[j] is None:
+                assert y_pos[j] is None
+                if lab == 'r':
+                    x_pos[j] = x_pos[i] - 1
+                    y_pos[j] = y_pos[i]
+                else:
+                    x_pos[j] = x_pos[i]
+                    y_pos[j] = y_pos[i] - 1
+                todo.append(j)
+
+    return x_pos, y_pos
+
+
+def square_tiled_plot(r, u, G):
     r"""
-    Return a plot of the origami ``(r, u)`` using a soft layout (if any).
+    Return a plot of the origami ``(r, u)`` using the layout determined by the subgraph ``G``.
     """
     n = len(r)
     r_inv = [None] * n
@@ -123,26 +193,31 @@ def soft_plot(r, u):
         r_inv[r[i]] = i
         u_inv[u[i]] = i
 
-    ans = soft_layout(o)
-    if ans is None:
-        raise ValueError('no soft layout')
-    G, x, y, r_cut, u_cut = ans
+    x, y = positions_from_layout(r, u, G)
+
     Gr = Graphics()
-    for i,(xi,yi) in enumerate(zip(x,y)):
-        Gr += polygon2d([(xi,yi),(xi+1,yi),(xi+1,yi+1),(xi,yi+1)], color=(0,0,1), alpha=0.1)
-        Gr += text(str(i+1), (xi+.5, yi+.5), color=(0,0,0), fontsize=11)
-        if r_cut[i]:
-            Gr += line2d([(xi+1,yi), (xi+1,yi+1)], color=(0,0,0))
+    for i in range(n):
+        Gr += polygon2d([(x[i],y[i]), (x[i]+1,y[i]), (x[i]+1,y[i]+1), (x[i],y[i]+1)], color=(0,0,1), alpha=0.1)
+        Gr += text(str(i+1), (x[i]+.5, y[i]+.5), color=(0,0,0), fontsize=11)
+
+        if (x[r[i]], y[r[i]]) != (x[i] + 1, y[i]):
+            # r-cut
+            Gr += line2d([(x[i]+1,y[i]), (x[i]+1,y[i]+1)], color=(0,0,0))
         else:
-            Gr += line2d([(xi+1,yi), (xi+1,yi+1)], color=(.3,.3,.3), linestyle='dashed')
-        if u_cut[i]:
-            Gr += line2d([(xi,yi+1),(xi+1,yi+1)], color=(0,0,0))
+            Gr += line2d([(x[i]+1,y[i]), (x[i]+1,y[i]+1)], color=(.3,.3,.3), linestyle='dashed')
+
+        if (x[u[i]], y[u[i]]) != (x[i], y[i] + 1):
+            # u-cut
+            Gr += line2d([(x[i],y[i]+1),(x[i]+1,y[i]+1)], color=(0,0,0))
         else:
-            Gr += line2d([(xi,yi+1),(xi+1,yi+1)], color=(.3,.3,.3), linestyle='dashed')
-        if r_cut[r_inv[i]]:
-            Gr += line2d([(xi,yi), (xi,yi+1)], color=(0,0,0))
-        if u_cut[u_inv[i]]:
-            Gr += line2d([(xi,yi), (xi+1,yi)], color=(0,0,0))
+            Gr += line2d([(x[i],y[i]+1),(x[i]+1,y[i]+1)], color=(.3,.3,.3), linestyle='dashed')
+
+        if (x[r_inv[i]], y[r_inv[i]]) != (x[i] - 1, y[i]):
+            # r-cut
+            Gr += line2d([(x[i],y[i]), (x[i],y[i]+1)], color=(0,0,0))
+        if (x[u_inv[i]], y[u_inv[i]]) != (x[i], y[i] - 1):
+            # u-cut
+            Gr += line2d([(x[i], y[i]), (x[i]+1, y[i])], color=(0,0,0))
 
     Gr.axes(False)
     Gr.set_aspect_ratio(1)
